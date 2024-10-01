b135a4ab90a477e4ba6d12a1310ebc46d8a8f6c6
eloisasouza2807 <nsouza.eloisa@gmail.com>
2024-09-30 21:53:31 -0400

    Update Projeto_Calculadora_IMC.py

--- Projeto_Calculadora_IMC.py
+++ Projeto_Calculadora_IMC.py
@@ -2,17 +2,13 @@
 #Data:Setembro 2024
 #Projeto 01
 
-#Peso do usuário
 peso = int(input('Insira o seu peso:'))
-#Altura
 altura = float(input('Insira a sua altura:'))
-#Calcular IMC
 imc = peso / altura**2
-#Imprimir o IMC
 resultado = imc
 
 if resultado < 18.5:
-    print('Seu IMC é: Abaixo do e_peso')
+    print('Seu IMC é: Abaixo do peso')
 elif 18.5 and resultado < 25:
     print('Seu IMC é: Normal')
 elif 25 and resultado < 30:
